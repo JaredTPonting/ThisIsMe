34313d31138dd6a957443d46c4a90c02f78496f6
Jared <jaredtponting@gmail.com>
2024-10-03 19:41:48 +0100

    current

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,19 @@
+import React from "react"
+import "../styles/Home.css"
 
-function Home() {
-    return <div>Home</div>
-}
+
+const Home = () => {
+    return (
+      <div className="home-container">
+        <div className="description-box">
+          <h2>Hello, I'm Jared</h2>
+          <p>Welcome to my portfolio website! I'm a Python developer and data professional with experience in software engineering, data analysis, and networking. Explore my projects, blog, and more.</p>
+        </div>
+        <div className="image-container">
+          <img src="../src/assets/img/kyoto.jpg" alt="Your Name" className="profile-pic" />
+        </div>
+      </div>
+    );
+  };
 
 export default Home
