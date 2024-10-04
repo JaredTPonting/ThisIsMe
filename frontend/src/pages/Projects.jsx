1a26f6c66f918a7620c827ca6d7ef2a7e71af9ea
Jared <jaredtponting@gmail.com>
2024-10-04 20:47:44 +0100

    backend

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+import React from "react"
+import axios from 'axios';
+
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/projects/")
+        .then(response => {
+            setProjects(response.data);
+            console.log(projects)
+        })
+        .catch(err => {console.log("There was an error fetching the projects!", err);
+        });
+    }, []);
+
     return <div>Projects</div>
 }
 
