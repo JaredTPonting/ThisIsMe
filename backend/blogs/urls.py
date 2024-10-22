6115ea1948c8ad20408dd98f479a043d7836bdc6
Jared <jaredtponting@gmail.com>
2024-10-22 15:39:18 +0100

    working backup

--- backend/blogs/urls.py
+++ backend/blogs/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from .views import BlogPostListCreateView, BlogPostDetailView
+
+router = DefaultRouter()
 
 urlpatterns = [
     path('posts/', BlogPostListCreateView.as_view(), name='blog-list-create'),
