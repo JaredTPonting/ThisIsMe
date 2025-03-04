2dbd89a2388080b2b58720b84d85a549deda3d0a
Jared <jaredtponting@gmail.com>
2025-03-04 21:03:53 +0000

    restart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,16 @@ import profileImage from "../assets/img/kyoto.jpg"
 const Home = () => {
     return (
       <div className="home-container">
-        <div className="image-container">
+        <div className="profile-image-container">
           <img src={profileImage} alt="Your Name" className="profile-pic" />
         </div>
         <div className="description-box">
           <h2>Hello, I'm Jared</h2>
           <p>Welcome to my portfolio website! I'm a Python developer and data professional with experience in software engineering, data analysis, and networking. Explore my projects, blog, and more.</p>
         </div>
-        
+        <div className="description-box-two">
+            <h2>Hello</h2>
+        </div>
       </div>
     );
   };
