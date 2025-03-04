7a1a07edd2dd8cde378fde5cbf9488f590c22037
Jared <jaredtponting@gmail.com>
2025-03-04 22:12:34 +0000

    restart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,38 @@
-import React from "react"
-import "../styles/Home.css"
-import profileImage from "../assets/img/kyoto.jpg"
-
+import React from "react";
+import "../styles/Home.css";
+import profileImage from "../assets/img/kyoto.jpg";
 
 const Home = () => {
-    return (
-      <div className="home-container">
-        <div className="profile-image-container">
-          <img src={profileImage} alt="Your Name" className="profile-pic" />
+  return (
+    <div className="home-container">
+      <div className="profile-section">
+        <img src={profileImage} alt="Profile" className="profile-pic" />
+      </div>
+      <div className="text-boxes">
+        <div className="text-box">
+          <h2>Header 1</h2>
+          <p>
+            This is the content for the first text box. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit.
+          </p>
         </div>
-        <div className="description-box">
-          <h2>Hello, I'm Jared</h2>
-          <p>Welcome to my portfolio website! I'm a Python developer and data professional with experience in software engineering, data analysis, and networking. Explore my projects, blog, and more.</p>
+        <div className="text-box">
+          <h2>Header GAY</h2>
+          <p>
+            This is the content for the second text box. Suspendisse potenti.
+            Curabitur nec.
+          </p>
         </div>
-        <div className="description-box-two">
-            <h2>Hello</h2>
+        <div className="text-box">
+          <h2>Header 3</h2>
+          <p>
+            This is the content for the third text box. Praesent vel magna
+            efficitur, aliquet massa at, placerat nisi.
+          </p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default Home
+export default Home;
