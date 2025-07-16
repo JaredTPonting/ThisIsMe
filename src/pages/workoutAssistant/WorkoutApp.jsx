264732b4d206bf081ab2ae654f1056b40d5e16ee
Jared <jaredtponting@gmail.com>
2025-07-16 18:52:07 +0100

    workout app page

---
+++ src/pages/workoutAssistant/WorkoutApp.jsx
@@ -0,0 +1,16 @@
+const WorkoutApp = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Welcome to the Workout Assistant 💪</h2>
+      <p>This is where i will be building the workout app</p>
+      <ul>
+        <li>Workout tracking</li>
+        <li>Timer or rest alerts</li>
+        <li>Progress logging</li>
+        <li>Suggested routines</li>
+      </ul>
+    </div>
+  );
+};
+
+export default WorkoutApp;
