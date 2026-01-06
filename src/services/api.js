3d342bb14bb0a9dc533c22ebf18d11ed02ebba20
Jared <jaredtponting@gmail.com>
2026-01-06 19:03:11 +0000

    ur

--- src/services/api.js
+++ src/services/api.js
@@ -18,7 +18,7 @@ export const apiFetch = async (url, options = {}) => {
 
 // Projects
 
-export const getTags = () => apiFetch("/api/tags/");
+export const getTags = () => apiFetch("/tags/");
 
-export const getProjects = () => apiFetch("/api/projects/");
+export const getProjects = () => apiFetch("/projects/");
 
